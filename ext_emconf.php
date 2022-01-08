529a4503dff857ebd37a975ebdbe6209b3cc96b9
Georg Ringer <georg.ringer@gmail.com>
2022-01-08 20:36:38 +0100

    [TASK] Allow v11
    Allow version 11.
    
    Resolves: #3
    Co-Authored-By: David Bruchmann <david.bruchmann@gmail.com>

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,10 +8,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'mail@ringer.it',
     'state' => 'beta',
     'clearCacheOnLoad' => true,
-    'version' => '1.0.1',
+    'version' => '1.0.2',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-10.4.99',
+            'typo3' => '9.5.0-11.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
